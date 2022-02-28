6637b6200ff3d14f5df9d67f017f330e56bcc166
Khalidprithy <khalidprithy@gmail.com>
2022-03-01 00:49:40 +0600

    Error message added

--- js/phone.js
+++ js/phone.js
@@ -1,35 +1,66 @@
+// search input data
+
 const searchPhones = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
-
+    // clear input
     searchField.value = '';
 
-    const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhoneResults(data.data))
+    if (searchText == '') {
+        const noInput = document.getElementById('no-input');
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <span class="bg-danger p-1 rounded-3">Please search a Phone</span>
+        `;
+        noInput.appendChild(div);
+    }
+    else {
+        // fetch input data
+        const url = ` https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayPhoneResults(data.data))
+
+    }
+
 }
 
 const displayPhoneResults = phones => {
     const searchResults = document.getElementById('search-results');
-    phones.forEach(phone => {
-        console.log(phone);
+    searchResults.textContent = '';
+
+
+    if (phones.length === 0) {
+        const noResult = document.getElementById('no-result');
         const div = document.createElement('div');
-        div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100 border-0">
-            <img src="${phone.image}" class="card-img-top w-50" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${phone.phone_name}</h5>
-              <p class="card-text">Brand: ${phone.brand}</p>
-              <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="btn btn-outline-primary  btn-sm">Details</button>
-            </div>
-        </div>
-        
+        <span class="bg-danger p-1 rounded-3">No result found</span>
         `;
-        searchResults.appendChild(div);
-    })
+        noResult.appendChild(div);
+    }
+    else {
+        phones.forEach(phone => {
+            console.log(phone);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100 border-0">
+                <img src="${phone.image}" class="card-img-top w-50" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${phone.phone_name}</h5>
+                  <p class="card-text">Brand: ${phone.brand}</p>
+                  <button onclick="loadPhoneDetails('${phone.slug}')" type="button" class="btn btn-outline-primary  btn-sm">Details</button>
+                </div>
+            </div>
+            
+            `;
+            searchResults.appendChild(div);
+        })
+    }
+
+
+
 }
 
 const loadPhoneDetails = phoneId => {
